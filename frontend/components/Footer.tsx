7baf5d9976f2831b61a80f68f87d2bb468ca8a0b
Joshua Zobule <64586778+thisisjoshu@users.noreply.github.com>
2024-07-28 19:32:39 +1100

    footer ui complete

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,6 +1,9 @@
 function Footer() {
     return ( 
-        <></>
+        <div className="py-8 px-2 text-center text-text-normal text-purple-200">
+            <p>built with &#10084; by Rebays</p>
+            <p>copyright 2024. All rights reserved</p>
+        </div>
      );
 }
 
