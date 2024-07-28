c6a0820941b1d5574a328be31f95d9edbc871dc9
Joshua Zobule <64586778+thisisjoshu@users.noreply.github.com>
2024-07-28 20:11:02 +1100

    UI information component completed

--- frontend/components/Information.tsx
+++ frontend/components/Information.tsx
@@ -1,7 +1,16 @@
 function Information() {
     return ( 
-        <>
-        </>
+        <div className="py-14 px-8 space-y-12">
+            <div className="space-y-8">
+                <h5 className="text-text-title text-purple-500 font-bold">Stay on top of your tax</h5>
+                <p>If you're earning an income, you'll probably need to pay income tax. How much you pay depends on how much you earn.</p>
+            </div>
+            <div className="space-y-8">            
+                <h6 className="text-text-sub-title text-purple-500 font-bold">Calculate your tax</h6>
+                <p>To see how much tax is deducted you can use the calculator.</p>
+                <p>If you're an employee, your employer will deduct tax from each pay and send it to IRD.</p>
+            </div>
+        </div>
      );
 }
 
