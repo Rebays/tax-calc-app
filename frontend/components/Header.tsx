a8990ef93c74ecf5fae7a686f7743b1a37c4c15f
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2024-07-28 19:24:38 +1100

    update Header component

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image";
+import logo from "@/assets/tax-calc-logo.svg"
+
 function Header() {
     return ( 
-        <></>
+        <header className="ps-8 pt-6 pb-6">
+            <Image
+                src={logo} 
+                alt="logo" 
+            />            
+        </header>
      );
 }
 
