81f786bcb129dc469f73cd0c007870e36d98901d
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2024-07-28 19:56:26 +1100

    update Hero component

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -1,6 +1,13 @@
 function Hero() {
     return ( 
-        <></>
+        <div className="bg-hero-gradient min-h-[400px] flex flex-col items-center align-middle justify-center gap-10">
+            <h3 className="text-white text-text-heading inline-block text-center ps-2 pe-2 ">
+                Income Tax Calculator
+            </h3>
+            <button className=" capitalize font-bold pt-4 pb-4 ps-6 pe-6 bg-white text-purple-800 rounded-2xl">
+                calculate now
+            </button>
+        </div>
      );
 }
 
