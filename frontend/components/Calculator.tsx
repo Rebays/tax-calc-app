7d13ba7388c4ea9f1c1f18c9e084379dcd7e4a54
Joshua Zobule <64586778+thisisjoshu@users.noreply.github.com>
2024-08-01 12:26:12 +1100

    fixed padding

--- frontend/components/Calculator.tsx
+++ frontend/components/Calculator.tsx
@@ -4,14 +4,13 @@ import CalculatorSummary from "./CalculatorSummary";
 
 function Calculator() {
     return ( 
-        <div className="bg-green-100 py-14">
-        <CalculatorInput/>
-        <div className="px-4" id="calculator-result-box">
-            <CalculatorResult/>
-            <CalculatorSummary/>
-        
-       </div>
-
+        <div className="bg-green-100 px-8 py-16 space-y-12">
+            <CalculatorInput/>
+            <div id="calculator-result-box">
+                <CalculatorResult/>
+                <CalculatorSummary/>
+            
+            </div>
         </div>
      );
 }
