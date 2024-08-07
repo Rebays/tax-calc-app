a4cc79eff293c6f520b0a6bb55274ad27b4bcde6
Geolynx Saemane <gsaemane@genisys.com.sb>
2024-08-07 22:17:26 +1100

    Create Accordion as Reusable Component
    Break up accordion component from Extras to it's reusable version.

--- frontend/ui/Accordion.tsx
+++ frontend/ui/Accordion.tsx
@@ -1,5 +1,26 @@
-function Accordion() {
+import { useState } from "react";
+import { MinusIcon, PlusIcon } from "@heroicons/react/16/solid";
+
+{/**
+    NOTE TO DEV: 
+    Render this as client side component
+    props: 
+        title   -   item title/summary, 
+        content -   item description
+    usage:
+        <Accordian title="Info" content="whatever"/>
+    */}
+function Accordion({title="Title", content ="Awesome content"}) {
+    const [isExpanded, setIsExpanded] = useState(false)
     return ( <>
+        <details className="group py-2" onClick={()=>setIsExpanded(pIsExpanded=>!pIsExpanded)}>
+            <summary className="relative flex items-center rounded-2xl  justify-between w-full p-6 font-semibold text-left transition-all ease-in border-b  cursor-pointer text-white bg-purple-500">
+                {title}
+
+                {isExpanded? <MinusIcon className="absolute right-2 text-text-normal w-6 roup-open:opacity-100"/> : <PlusIcon className="absolute right-2 text-text-normal w-6 roup-open:opacity-100" /> }        
+            </summary>
+            {isExpanded && <div className="p-4 text-neutral-600">{content}</div>}
+        </details>
     </> );
 }
 
