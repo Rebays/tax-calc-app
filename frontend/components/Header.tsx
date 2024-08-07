215fbcd026fdfbd5019b4b6521feddff7f88485c
Brandon Ryan Tupiti <tupitibrandon@gmail.com>
2024-08-07 23:21:40 +1100

    update Header layout

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 import logo from "@/assets/tax-calc-logo.svg"
+import Container from "@/ui/Container";
 
 function Header() {
     return ( 
-        <header className="ps-8 pt-6 pb-6 bg-purple-600">
-            <Image
-                src={logo} 
-                alt="logo" 
-            />            
+        <header className=" bg-purple-600">
+            <Container>
+                <div className="ps-8 pt-6 pb-6">
+                    <Image
+                        src={logo} 
+                        alt="logo" 
+                    /> 
+                </div>        
+            </Container>   
         </header>
      );
 }
